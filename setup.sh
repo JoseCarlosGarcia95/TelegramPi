41b3a72e17c132296a132e455cc00c11cd317a95
JoseCarlosGarcia95 <josecarlos.garciaortega@alum.uca.es>
2017-11-04 10:48:05 +0100

    Made some improvements in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,29 @@
-sudo apt install python-configparser
-sudo apt install python-nmap nmap
+#!/bin/bash
 
-if test -r config.ini
+# Packages needed for TelegramPi
+PACKAGES=("python-configparser" "python-nmap" "nmap")
+
+for PACKAGE in ${PACKAGES[*]}; do
+    if [ $(dpkg-query -W -f='${Status}' $PACKAGE 2>/dev/null | grep -c "ok installed") -eq 0 ];
+    then
+        sudo apt install $PACKAGE
+    fi
+done
+
+# Configuration generator.
+if [[ ! -f config.ini ]];
 then
-echo "Realiza el siguiente paso"
-else
-touch config.ini
-chmod 777 config.ini
-echo "Configura el archivo config.ini según la guía"
+    echo -n "Please enter telegram username of super user [ENTER]: "
+    read SUPER_USER
+    
+    echo -n "Please enter telegram token and then [ENTER]: "
+    read TELEGRAM_TOKEN
+
+    echo -n "Please enter a path for downloading files [ENTER]: "
+    read DOWNLOAD_PATH
+
+    CONFIG_INI="[Telegram-Info]\nbot-token=$TELEGRAM_TOKEN\nupdate-offset=0\nsuper_user=$SUPER_USER\n[Modules]\ndownload-file-path=$DOWNLOAD_PATH"
+
+    echo -e "$CONFIG_INI" > config.ini
+    chmod 777 config.ini
 fi
